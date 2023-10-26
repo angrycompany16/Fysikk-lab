458044b891a7223606c4e79038ec782f9b82aec0
AndreasOnsdagFem <anyseter@gmail.com>
2023-10-26 10:37:54 +0200

    feaoif

--- fem.py
+++ fem.py
@@ -1,5 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np 
+
 fem = 5 +5
-boy = [5,4,3,6,78,4,666]
+boy = [5,4,3,6,78,4,66]
 
 for fem in boy:
     print(fem)
+
+def k(p):
+    p**2
+
+x = np.linspace(-5,5,7)
+
+
+plt.plot(x, boy)
+plt.show()
